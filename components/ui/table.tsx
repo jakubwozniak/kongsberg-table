75e8d08cfd8b67c320ed93c0b442d570328b5dac
Jakub <kwozniak992@gmail.com>
2024-06-21 20:57:43 +0200

    improve UI

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -18,7 +18,14 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b-0", className)} {...props} />
+  <thead
+    ref={ref}
+    className={cn(
+      "[&_tr]:border-b-0 shadow-[0_0px_5px_2px_rgba(0,0,0,0.06)]",
+      className
+    )}
+    {...props}
+  />
 ));
 TableHeader.displayName = "TableHeader";
 
@@ -67,7 +74,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-2 last:px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 text-sm md:text-base",
+      "h-12 px-2 last:px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 text-sm md:text-base ",
       className
     )}
     {...props}
